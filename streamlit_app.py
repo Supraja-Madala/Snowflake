f3a383645ca838b08bd50bc2b0020cf6f4ec6cc3
Supraja-Madala <117875014+Supraja-Madala@users.noreply.github.com>
2022-11-14 14:54:43 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,8 +30,8 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+#streamlit.text(fruityvice_response.json())
 
 # normalises json data to table format 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())

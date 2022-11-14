c87fa770c137e4986a0da2d300d784bd2116a5af
Supraja-Madala <117875014+Supraja-Madala@users.noreply.github.com>
2022-11-14 14:56:17 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,8 +30,11 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 #streamlit.text(fruityvice_response.json())
+
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 # normalises json data to table format 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
